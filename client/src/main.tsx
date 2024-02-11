a758aa884417de3991ada4dc2fa03f27ea6e5545
duymanh3602 <82275607+duymanh3602@users.noreply.github.com>
2024-02-12 01:19:47 +0700

    refactor: format code

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import './index.css'
 import App from './App.tsx'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
